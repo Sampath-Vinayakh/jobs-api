90700c201e299835a13bc5ebf48149618e4a3bed
Sampath J <jsampath206@gmail.com>
2023-10-17 17:03:28 +0530

    swagger-docs added

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // connectDB
 const connectDB = require("./db/connect");
 
@@ -38,8 +44,11 @@ app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
-  res.send("Jobs API");
+  res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>');
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
